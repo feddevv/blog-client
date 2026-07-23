faac0e910a31e9e8b61993099bfd1d338156b8a6
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-23 17:38:56 +0000

    feat: implement Input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,28 @@
+import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { type InputHTMLAttributes } from 'react';
+
+const inputVariants = cva(
+  'flex w-full bg-transparent text-sm font-paragraph font-light border-0 outline-none focus:outline-none focus:ring-0 focus-visible:outline-none focus-visible:ring-0 shadow-none file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
+  {
+    variants: {
+      intent: {
+        unstyled: '',
+        primary:
+          'bg-white text-foreground p-2 border border-border placeholder:text-[14px]',
+      },
+    },
+    defaultVariants: {
+      intent: 'unstyled',
+    },
+  }
+);
+
+type InputProps = VariantProps<typeof inputVariants> &
+  InputHTMLAttributes<HTMLInputElement>;
+
+export default function Input({ className, intent, ...props }: InputProps) {
+  return (
+    <input {...props} className={cn(inputVariants({ intent, className }))} />
+  );
+}
