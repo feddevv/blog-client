6ccde548eb6447b45f3346d5a6c3ed821706a2ba
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-23 13:12:45 +0000

    test: add tests for Header component

---
+++ src/tests/header.test.tsx
@@ -0,0 +1,46 @@
+import Header from '@/layout/Header';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { createRoutesStub, Outlet } from 'react-router';
+import { describe, expect, it } from 'vitest';
+
+describe('Header component', () => {
+  const RouterStub = createRoutesStub([
+    {
+      path: '/',
+      Component: () => {
+        return (
+          <>
+            <Header />
+            <main>
+              <Outlet />
+            </main>
+          </>
+        );
+      },
+      children: [
+        {
+          index: true,
+          Component: () => <h2>Home page</h2>,
+        },
+        {
+          path: '/about',
+          Component: () => <h2>About page</h2>,
+        },
+      ],
+    },
+  ]);
+
+  it('should redirect to another page when click the link', async () => {
+    const user = userEvent.setup();
+
+    render(<RouterStub initialEntries={['/']} />);
+
+    const aboutLink = screen.getByRole('link', { name: /about/i });
+    await user.click(aboutLink);
+
+    expect(
+      screen.getByRole('heading', { name: /about page/i })
+    ).toBeInTheDocument();
+  });
+});
