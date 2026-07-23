2c6fb63c9f107d8697d90b7f8fc965a9d710808d
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-23 17:11:29 +0000

    feat: implement Label component

---
+++ src/components/Label.tsx
@@ -0,0 +1,41 @@
+import { cn } from '@/lib/utils';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { type LabelHTMLAttributes } from 'react';
+
+const labelVariants = cva('font-paragraph font-medium', {
+  variants: {
+    intent: {
+      wrapper: 'flex items-center gap-1',
+      primary: 'text-foreground',
+    },
+    size: {
+      sm: 'text-[12px]',
+      md: 'text-[16px]',
+      xl: 'text-xl',
+    },
+  },
+
+  defaultVariants: {
+    intent: 'primary',
+  },
+});
+
+type LabelProps = VariantProps<typeof labelVariants> &
+  LabelHTMLAttributes<HTMLLabelElement>;
+
+export default function Label({
+  children,
+  className,
+  intent,
+  size,
+  ...props
+}: LabelProps) {
+  return (
+    <label
+      {...props}
+      className={cn(labelVariants({ intent, size, className }))}
+    >
+      {children}
+    </label>
+  );
+}
