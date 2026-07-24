ff08171672163e6667023a05b91562b6f9c39916
Fedya Styopochkin <fst20071221@gmail.com>
2026-07-24 14:55:23 +0000

    feat: implement Card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,39 @@
+import { LuHeart } from 'react-icons/lu';
+
+interface CardProps {
+  img: string;
+  title: string;
+  description: string;
+  likes: number;
+}
+
+export default function Card({ img, title, description, likes }: CardProps) {
+  return (
+    <article className="border border-border inline-flex flex-col bg-white cursor-pointer group/card hover:shadow hover:-translate-y-1 transition-all duration-250">
+      <div className="overflow-hidden w-full">
+        <img
+          src={img}
+          width={350}
+          height={250}
+          className="w-full group-hover/card:scale-105 object-cover transition-transform duration-250"
+          alt=""
+        />
+      </div>
+      <div className="p-4 flex flex-col">
+        <h3 className="font-heading font-bold text-[18px] mb-2 group-hover/card:text-accent transition-colors duration-250">
+          {title}
+        </h3>
+        <p className="text-sm text-muted-foreground">{description}</p>
+
+        <div className="h-px w-ull bg-muted my-4 self-stretch"></div>
+
+        <div className="ml-auto self-start flex items-center gap-1 cursor-pointer group/likes">
+          <LuHeart className="text-sm text-muted-foreground group-hover/likes:text-destructive" />
+          <p className="text-sm text-muted-foreground group-hover/likes:text-destructive">
+            {likes}
+          </p>
+        </div>
+      </div>
+    </article>
+  );
+}
