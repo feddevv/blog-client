170f9375772dd67ea65ef62de257a5969c8f0e29
Fedya Styopochkin <fst20071221@gmail.com>
2026-08-13 13:45:14 +0000

    feat: allow set and send cookies by setting withCredentials option

--- src/services/config.ts
+++ src/services/config.ts
@@ -4,6 +4,7 @@ import type { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 export const blogApi: AxiosInstance = axios.create({
   baseURL: 'https://blog-api-65st.onrender.com',
   timeout: 5000,
+  withCredentials: true,
 });
 
 blogApi.interceptors.request.use((config: InternalAxiosRequestConfig) => {
