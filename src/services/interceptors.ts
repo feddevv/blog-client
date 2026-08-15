2d3f4da9133f913b82accbfaf34ea1eff4ad1593
Fedya Styopochkin <fst20071221@gmail.com>
2026-08-15 13:50:09 +0000

    fix: add lists for skipping auth urls when intercepting 401 errors

--- src/services/interceptors.ts
+++ src/services/interceptors.ts
@@ -9,6 +9,8 @@ interface QueueItem {
 let isRefetching = false;
 let failedQueue: QueueItem[] = [];
 
+const URL_SKIP_LIST = ['/auth/register', '/auth/login'];
+
 function processQueue(err: unknown, token: string | null = null) {
   failedQueue.forEach((item) => {
     if (err) item.reject(err);
@@ -34,40 +36,47 @@ export const setupInterceptors = (instance: AxiosInstance) => {
     async (error) => {
       const originalRequest = error.config;
 
-      if (error.response?.status === 401 && !originalRequest._retry) {
-        if (isRefetching) {
-          return new Promise((resolve, reject) => {
-            failedQueue.push({ resolve, reject });
-          })
-            .then(() => instance(originalRequest))
-            .catch((err) => Promise.reject(err));
-        }
-
-        originalRequest._retry = true;
-        isRefetching = true;
-
-        try {
-          const response = await axios.post<{ token: string }>(
-            'http://localhost:3000/api/auth/refresh',
-            {},
-            { withCredentials: true }
-          );
-
-          processQueue(null, response.data.token);
-          localStorage.setItem('token', response.data.token);
-
-          return instance(originalRequest);
-        } catch (refreshError) {
-          processQueue(refreshError);
-
-          localStorage.removeItem('token');
-          return Promise.reject(refreshError);
-        } finally {
-          isRefetching = false;
-        }
+      const isToSkip = URL_SKIP_LIST.some((url) =>
+        originalRequest.url.includes(url)
+      );
+      if (
+        error.response?.status !== 401 ||
+        originalRequest._retry ||
+        isToSkip
+      ) {
+        return Promise.reject(error);
       }
 
-      return Promise.reject(error);
+      if (isRefetching) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then(() => instance(originalRequest))
+          .catch((err) => Promise.reject(err));
+      }
+
+      originalRequest._retry = true;
+      isRefetching = true;
+
+      try {
+        const response = await axios.post<{ token: string }>(
+          'http://localhost:3000/api/auth/refresh',
+          {},
+          { withCredentials: true }
+        );
+
+        processQueue(null, response.data.token);
+        localStorage.setItem('token', response.data.token);
+
+        return instance(originalRequest);
+      } catch (refreshError) {
+        processQueue(refreshError);
+
+        localStorage.removeItem('token');
+        return Promise.reject(refreshError);
+      } finally {
+        isRefetching = false;
+      }
     }
   );
 };
