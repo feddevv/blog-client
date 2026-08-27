f25fb04117db959d8b8a1ac91941aa854fbc1815
Fedya Styopochkin <fst20071221@gmail.com>
2026-08-27 13:20:15 +0000

    fix: add type definition for response in togglePostLike function

--- src/services/postLikes.ts
+++ src/services/postLikes.ts
@@ -1,7 +1,7 @@
 import { blogApi } from './config';
 
 export const togglePostLike = async ({ id }: { id: number }) => {
-  const res = await blogApi.post(`/api/posts/${id}/likes`);
+  const res = await blogApi.post<{ message: string }>(`/api/posts/${id}/likes`);
 
   return res.data;
 };
