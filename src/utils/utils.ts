13589518da8e04f1ea919e67e9f7c0cff83dae06
Fedya Styopochkin <fst20071221@gmail.com>
2026-09-09 14:13:38 +0000

    feat: add calculateReadingTime utility and corresponding tests

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -30,3 +30,11 @@ export function range(start: number, end: number) {
   const length = end - start + 1;
   return Array.from({ length }, (_, i) => start + i);
 }
+
+export function calculateReadingTime(wordsAmount: number) {
+  if (wordsAmount < 0) return 0;
+
+  const AVERAGE_WPM = 250;
+
+  return Math.ceil(wordsAmount / AVERAGE_WPM);
+}
