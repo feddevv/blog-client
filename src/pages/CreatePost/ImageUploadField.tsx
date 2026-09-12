5ba07e5bcbd250b2040424c37eb9597290f47f56
Fedya Styopochkin <fst20071221@gmail.com>
2026-09-12 14:13:58 +0000

    feat: prevent dragging all file types except images

--- src/pages/CreatePost/ImageUploadField.tsx
+++ src/pages/CreatePost/ImageUploadField.tsx
@@ -28,6 +28,19 @@ export default function ImageUploadField({ register }: ImageUploadFieldProps) {
       ref={dropZone}
       htmlFor="post-image-file"
       className={`bg-card border border-border p-5 rounded-xs flex flex-col gap-4`}
+      onDragOver={(e) => {
+        const fileItems = [...e.dataTransfer.items].filter(
+          (item) => item.kind === 'file'
+        );
+
+        if (fileItems.length > 0) {
+          e.preventDefault();
+
+          if (fileItems.some((item) => item.type.startsWith('image/'))) {
+            e.dataTransfer.dropEffect = 'copy';
+          } else e.dataTransfer.dropEffect = 'none';
+        }
+      }}
     >
       <span className="flex items-center gap-1.5 font-bold uppercase tracking-wider">
         <LuImage className="text-accent text-sm" />
