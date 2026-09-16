4850f308b9773254c27d577491c6e7f70fe4b8d7
Fedya Styopochkin <fst20071221@gmail.com>
2026-09-16 14:16:18 +0000

    feat: implement form submission and draft saving in UpdatePost

--- src/pages/UpdatePost/UpdatePost.tsx
+++ src/pages/UpdatePost/UpdatePost.tsx
@@ -1,19 +1,43 @@
 import PostForm from '@/components/PostForm';
 import Spinner from '@/components/Spinner';
-import { usePostById } from '@/hooks/usePosts';
+import { usePostById, useUpdatePostById } from '@/hooks/usePosts';
+import type { PostFormValues } from '@/types/zod';
 import { useParams } from 'react-router';
 
 export default function UpdatePost() {
   const { id } = useParams();
-  const { data: post, isPending } = usePostById(Number(id));
+  const { data: post, isPending: isPendingFetchPost } = usePostById(Number(id));
+  const { mutate, isPending: isPendingUpdatePost } = useUpdatePostById();
 
-  return isPending ? (
+  const onSubmit = (data: PostFormValues) => {
+    const formData = new FormData();
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+    formData.append('content', data.content);
+    formData.append('state', data.state);
+    formData.append('postImage', data.postImage[0]);
+
+    mutate({ id: Number(id), data: formData });
+  };
+
+  const handleSaveDraft = (data: PostFormValues) => {
+    const formData = new FormData();
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+    formData.append('content', data.content);
+    formData.append('state', 'DRAFT');
+    formData.append('postImage', data.postImage[0]);
+
+    mutate({ id: Number(id), data: formData });
+  };
+
+  return isPendingFetchPost ? (
     <Spinner className="m-auto" />
   ) : (
     <PostForm
-      handleSaveDraft={() => {}}
-      isPending={isPending}
-      onSubmit={() => {}}
+      handleSaveDraft={handleSaveDraft}
+      isPending={isPendingUpdatePost}
+      onSubmit={onSubmit}
       initialValues={{
         title: post?.title,
         content: post?.content,
