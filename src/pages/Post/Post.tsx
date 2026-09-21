51a7e823bed51a046a0976a2d7b466bb23d5fce7
Fedya Styopochkin <fst20071221@gmail.com>
2026-09-21 12:27:50 +0000

    refactor: move icon inside the NavLink in Post.tsx

--- src/pages/Post/Post.tsx
+++ src/pages/Post/Post.tsx
@@ -21,16 +21,13 @@ export default function Post() {
     <>
       <div className="sticky top-16 left-0 right-0 bg-background border-b border-border px-4 py-2">
         <div className="flex justify-between max-w-270 mx-auto">
-          <div className="text-muted-foreground flex items-center gap-2 group/back">
-            {/* TODO: CHANGE IT SO FOCUSED THE WHOLE CONTAINER, NOT JUST LINK */}
-            <IoIosArrowBack className="text-sm group-hover/back:text-foreground transition-colors duration-200" />
-            <NavLink
-              to={'/'}
-              className="group-hover/back:text-foreground transition-colors duration-200 text-[clamp(14px,2vw,16px)]"
-            >
-              All articles
-            </NavLink>
-          </div>
+          <NavLink
+            to={'/'}
+            className="text-muted-foreground hover:text-foreground transition-colors duration-200 text-[clamp(14px,2vw,16px)] flex items-center gap-2"
+          >
+            <IoIosArrowBack className="text-sm transition-colors duration-200" />
+            All articles
+          </NavLink>
 
           <div className="flex gap-4">
             <Like
